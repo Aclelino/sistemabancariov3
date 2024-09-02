f96020be217733c3856ebaf533c809dfdb417f42
aclelino <aclelino@gmail>
2024-09-02 18:54:55 -0300

    feature: criando as classes

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from abc import ABC , abstractclassmethod,abstractproperty
 
-class Conta:
-    pass
-class ContaCorrent(Conta):
 
-    pass
-class Historioco(Conta):
-
-    pass
 class Cliente:
+
     def __init__(self, endereco):
         self.endereco = endereco
         self.contas = []
@@ -17,6 +11,35 @@ class Cliente:
 
     def adicionar_conta(self,conta):
         self.contas.append(conta)
+
+
+class Conta:
+
+    def __init__(self,agencia:str,numero:int,cliente,historico):
+        self.agencia = agencia
+        self.cliente = cliente
+        self.historico = historico
+
+    def saldo(self):
+        pass
+
+    def nova_conta(self,cliente,numero):
+        pass
+
+    def sacar(valor:float,bool):
+        pass
+
+    def depositar(valor:float,bool):
+        pass
+    
+
+class ContaCorrent(Conta):
+
+    pass
+class Historioco(Conta):
+
+    pass
+
 
 class PessoaFisica(Cliente):
     pass
